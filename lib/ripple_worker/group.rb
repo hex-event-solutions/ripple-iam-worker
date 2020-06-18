812078007bd7c0203fe63c7f06868022819c938c
Ollie Nye <ollie@nyemail.co.uk>
2020-06-18 19:07:53 +0100

    All add_role and remove_role operations to group

--- lib/ripple_worker/group.rb
+++ lib/ripple_worker/group.rb
@@ -1,12 +1,30 @@
+# frozen_string_literal: true
+
 module RippleWorker
   class Group
     class << self
-      def create(body)
+      def create(**body)
         RippleWorker::Validate.validate([:name], body)
 
         response = RippleKeycloak::Group.create(body)
 
         puts "Created group #{response} from #{body}"
+      end
+
+      def add_role(**body)
+        RippleWorker::Validate.validate(%i[group_id role_name], body)
+
+        RippleKeycloak::Group.add_role(body[:group_id], body[:role_name])
+
+        puts "Added role #{body[:role_name]} to group #{body[:group_id]}"
+      end
+
+      def remove_role(**body)
+        RippleWorker::Validate.validate(%i[group_id role_name], body)
+
+        RippleKeycloak::Group.remove_role(body[:group_id], body[:role_name])
+
+        puts "Removed role #{body[:role_name]} from group #{body[:group_id]}"
       end
     end
   end
