941a63178d1e5e2e4c277871a2da50bb108dded7
Ollie Nye <ollie@nyemail.co.uk>
2020-06-18 19:07:36 +0100

    Require role

--- lib/ripple_worker.rb
+++ lib/ripple_worker.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require_relative 'ripple_worker/worker'
 require_relative 'ripple_worker/validate'
 require_relative 'ripple_worker/group'
 require_relative 'ripple_worker/user'
+require_relative 'ripple_worker/role'
 
 module RippleWorker
-
 end
